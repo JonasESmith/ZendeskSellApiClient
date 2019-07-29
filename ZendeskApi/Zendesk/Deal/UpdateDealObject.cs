1631236edd0bc3741c8111d40e2159fca438b4bd
Jonas Smith <pictofoster@gmail.com>
2019-07-29 13:38:27 -0500

    minor changes

--- ZendeskApi/Zendesk/Deal/UpdateDealObject.cs
+++ ZendeskApi/Zendesk/Deal/UpdateDealObject.cs
@@ -11,9 +11,6 @@ namespace Zendesk.Deal
 
   public class UpdateDealData
   {
-    public UpdateDealData() {
-      custom_fields = new UpdateDealCustom_Fields();
-    }
     public string   name                         { get; set; }
     public string   value                        { get; set; }
     public string   currency                     { get; set; }
@@ -27,12 +24,6 @@ namespace Zendesk.Deal
     public string   contact_id                   { get; set; }
     public object   estimated_close_date         { get; set; }
     public string[] tags                         { get; set; }
-    public UpdateDealCustom_Fields custom_fields { get; set; }
+    public DealCustom_Fields custom_fields { get; set; }
   }
-
-  public class UpdateDealCustom_Fields
-  {
-    public string website { get; set; }
-  }
-
 }
